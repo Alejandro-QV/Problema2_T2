9a244fda704c5aa0d0cd8990183a69589bee1fa1
Alejandro-QV <gabriel.qv1010@gmail.com>
2026-05-25 18:58:00 -0500

    avance1

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,3 +2,9 @@ def suma_recursiva(lista, pi, pf):
 
     if pi > pf:
         return 0
+    
+        return lista[pi] + suma_recursiva(lista, pi + 1, pf)
+lista = [2, 4, 6, 3]
+
+pi = int(input("Ingrese PI: "))
+pf = int(input("Ingrese PF: "))
