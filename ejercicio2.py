c0ca30c8c0c5f6ace20ceb5ade4fc3f19775e67c
Alejandro-QV <gabriel.qv1010@gmail.com>
2026-05-25 19:09:51 -0500

    solucion final problema 2 recursivo

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,6 +3,9 @@ def suma_recursiva(lista, pi, pf):
     if pi > pf:
         return 0
 
+    if pi >= len(lista):
+        return 0
+
     return lista[pi] + suma_recursiva(lista, pi + 1, pf)
 
 
@@ -15,8 +18,12 @@ for i in range(n):
 pi = int(input("Ingrese PI: "))
 pf = int(input("Ingrese PF: "))
 
+# 🔒 NORMALIZACIÓN OBLIGATORIA
 if pi > pf:
     pi, pf = pf, pi
+
+if pf >= len(lista):
+    pf = len(lista) - 1
 
 resultado = suma_recursiva(lista, pi, pf)
 
