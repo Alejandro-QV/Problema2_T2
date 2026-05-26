9c39c8046002772af6d7f1722887a20139a44849
Alejandro-QV <gabriel.qv1010@gmail.com>
2026-05-25 19:06:14 -0500

    version mejorada

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,10 +1,16 @@
 def suma_recursiva(lista, pi, pf):
 
-    if pi > pf or pi >= len(lista):
+    if pi > pf:
         return 0
 
     return lista[pi] + suma_recursiva(lista, pi + 1, pf)
-lista = [2, 4, 6, 3]
+
+
+n = int(input("Tamaño de la lista: "))
+lista = []
+
+for i in range(n):
+    lista.append(int(input("Numero: ")))
 
 pi = int(input("Ingrese PI: "))
 pf = int(input("Ingrese PF: "))
